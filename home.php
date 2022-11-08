b37ced399c89719ce3e4c8442bb679284a306283
Michael Encarnacion <maicol132603@gmail.com>
2022-11-07 21:19:49 -0400

    Se añadió un contenedor global al header

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
-<?php include "views/header.php" ?>
     <div class="viewLoader">
         <?php include "views/home/home.php" ?>
     </div>
+<?php include "views/header.php" ?>
 <?php include "views/footer.php" ?>
--- views/footer.php
+++ views/footer.php
@@ -1,8 +1,9 @@
-
+</div>
 <script src="src/js/viewLoader.js"></script>
 <script src="src/libs/datatable.js"></script>
 <script src="src/libs/all.min.js"></script>
 <script src="src/js/script.js"></script>
 <script src="src/libs/sweetalert.js"></script>
 </body>
+
 </html>
