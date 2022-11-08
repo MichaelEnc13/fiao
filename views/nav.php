c1871d42ba4a332875cff262e221b788eb8ef6ea
Michael Encarnacion <maicol132603@gmail.com>
2022-11-07 21:31:52 -0400

    Navbar

---
+++ views/nav.php
@@ -0,0 +1,13 @@
+<nav class="navbar">
+
+    <ul class="nav__wrapper">
+        <li id="router-home" class="nav__item">
+            Inicio
+        </li>
+        <li id="router-add" class="nav__item">
+            Nuevo
+        </li>
+
+    </ul>
+
+</nav>
