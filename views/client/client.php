5efe1950c482ac4e841c15d56e39f5f672229f4b
Steven <franciscomiguel1315@gmail.com>
2022-11-07 21:11:54 -0500

    Agregue estilos y elementos del views/client.php

--- views/client/client.php
+++ views/client/client.php
@@ -1,6 +1,45 @@
 <div class="home__container">
     <h2>Jhon Doe</h2>
     <div class="date__client">
-        <i>Total :</i>  <i>RD <span>000.00</span> DOP</i>
+        <i class="amout">Total : RD <span>000.00</span> DOP</i>
+    </div>
+    <div class="client_container">
+        <h3>Datos del cliente</h3>
+        <table id="table" class="table  table-hover">
+
+            <thead>
+                <tr>
+                    <td>id</td>
+                    <td>Monto total</td>
+                    <td>Fecha</td>
+                </tr>
+            </thead>
+
+            <tbody>
+
+                    <tr>
+                       <td>15</td>
+                       <td>RD 350.00 DOP</td>
+                       <td>07/02/22</td>
+                    </tr>
+                    <tr>
+                       <td>15</td>
+                       <td>RD 500.00 DOP</td>
+                       <td>22/03/22</td>
+                    </tr>
+                    <tr>
+                       <td>15</td>
+                       <td>RD 500.00 DOP</td>
+                       <td>22/03/22</td>
+                    </tr>
+                    <tr>
+                       <td>15</td>
+                       <td>RD 500.00 DOP</td>
+                       <td>22/03/22</td>
+                    </tr>
+             
+            </tbody>
+        </table>
+
     </div>
 </div>
