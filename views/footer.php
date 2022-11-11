b4ee31077bb6c33736ed040520994bc47757e89a
Michael Encarnacion <maicol132603@gmail.com>
2022-11-11 14:30:55 -0400

    Se ha agregado un control de versiones para los archivos de estilos y JS

--- views/footer.php
+++ views/footer.php
@@ -1,8 +1,8 @@
 
-<script src="src/js/viewLoader.js"></script>
+<script src="src/js/viewLoader.js<?php echo "?version=".$version?>"></script>
 <script src="src/libs/datatable.js"></script>
 <script src="src/libs/all.min.js"></script>
-<script src="src/js/script.js"></script>
+<script src="src/js/script.js<?php echo "?version=".$version?>"></script>
 <script src="src/libs/sweetalert.js"></script>
 </body>
 
--- views/header.php
+++ views/header.php
@@ -1,22 +1,22 @@
 <?php 
 if(session_status() != 2) session_start();
 $_SESSION['uid'] = 1;
-
+$version = 1.4;
 ?>
-
+<?php echo "?version=".$version?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="src/css/style.css">
+    <link rel="stylesheet" href="src/css/style.css<?php echo "?version=".$version?>">
     <link rel="stylesheet" href="src/libs/all.min.css">
     <script src="src/libs/jquery.min.js"></script>
     <link rel="stylesheet" href="src/libs/datatable.css">
     <link rel="shortcut icon" href="src/assets/iconos/favicon.png" type="image/x-icon">
     <script src="src/js/app.js"></script>
-    <link rel="manifest" href="manifest.json">
+    <link rel="manifest" href="manifest.json<?php echo "?version=".$version?>">
     <title>Fiao</title>
 </head>
 <body>
